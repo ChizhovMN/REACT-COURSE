63b9350827c5065ecf49a468e1ead4ea7554c07c
Chizhov Maxim <chizhov98ip@gmail.com>
2023-03-14 21:01:14 +0300

    feat:create 404,about and main page without layout

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Page404 extends React.Component {
+  render(): React.ReactNode {
+    return <div>404 Page</div>;
+  }
+}
+
+export default Page404;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+  render(): React.ReactNode {
+    return <div>MAIN PAGE</div>;
+  }
+}
+
+export default MainPage;
