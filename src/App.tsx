c04a81b5e6a7b6f25fe83fa40c03d603d27f991a
Chizhov Maxim <chizhov98ip@gmail.com>
2023-03-14 21:01:53 +0300

    feat:add switc for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import './App.css';
+import MainPage from './pages/main';
+import AboutPage from './pages/About';
+import Page404 from './pages/404';
 
 class App extends React.Component {
-  render() {
-    return <div className="App"></div>;
+  render(): React.ReactNode {
+    return (
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about_us" element={<AboutPage />} />
+          <Route path="/*" element={<Page404 />} />
+        </Routes>
+      </>
+    );
   }
 }
 
