ce4b146be89fe9ec45947f7df09949bec9810508
Chizhov Maxim <chizhov98ip@gmail.com>
2023-03-16 22:12:12 +0300

    feat:add json data for cards, create type for app props and state

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export type CardsType = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+};
+
+export type ProductsType = {
+  products: CardsType[];
+};
+
+export type AppStateProps = {
+  search: string;
+  location: string;
+  cards: ProductsType;
+};
