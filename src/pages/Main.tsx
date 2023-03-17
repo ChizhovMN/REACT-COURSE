940e9cfa0f7c63d31035199a91b413587a201345
Chizhov Maxim <chizhov98ip@gmail.com>
2023-03-17 16:39:57 +0300

    feat:add search bar with styles

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,11 +5,16 @@ import { ProductsType } from 'types';
 class MainPage extends React.Component<ProductsType> {
   render(): React.ReactNode {
     return (
-      <div className="card-table">
-        {this.props.products.map((card) => (
-          <Card key={card.id} {...card} />
-        ))}
-      </div>
+      <>
+        <div className="search-bar">
+          <input type="search" placeholder="Type here..." className="search" />
+        </div>
+        <div className="card-table">
+          {this.props.products.map((card) => (
+            <Card key={card.id} {...card} />
+          ))}
+        </div>
+      </>
     );
   }
 }
