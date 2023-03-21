388718f3f768f2610db1d1a50f0cd22ed7c3e796
Chizhov Maxim <chizhov98ip@gmail.com>
2023-03-21 22:06:21 +0300

    feat:add ref to input text,validation and error message

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,22 +1,68 @@
 import React from 'react';
+import { FormProps } from 'types';
 import CheckboxGroup from './CheckboxGroup';
 import DateInput from './DateInput';
+import ErrorMessage from './ErrorMessage';
 import RadioGroup from './RadioGroup';
 import Select from './Select';
+import Submit from './Submit';
 import TextInput from './TextInput';
 import Uploader from './Uploader';
 
-class Form extends React.Component {
+type FromProps = '';
+type FormState = {
+  textIsValid: boolean | null;
+};
+class Form extends React.Component<FormProps, FormState> {
+  public textInput: React.RefObject<HTMLInputElement>;
+  constructor(props: FormProps) {
+    super(props);
+    this.textInput = React.createRef();
+    this.state = {
+      textIsValid: null,
+    };
+  }
+  componentDidMount(): void {
+    console.log('component mouner');
+  }
+
+  onChangeText = () => console.log(this.textInput.current?.value);
   render(): React.ReactNode {
+    const { textIsValid } = this.state;
+    const validation = {
+      textInput: {
+        regExp: /^([A-Z]){1,}([a-z]){1,}[ ]{1}([A-Z]){1,}([a-z]){1,}[,]{1}[0-9]{1,}/g,
+        wrongMsg: 'Write you text right! Example: "Name Surname,1" ',
+      },
+    };
+    const checkValidation = (value: string, regExp: RegExp) => !!value.match(regExp);
     return (
-      <form className="form">
-        <TextInput />
-        <DateInput />
-        <Select />
-        <CheckboxGroup />
-        <RadioGroup />
-        <Uploader />
-      </form>
+      <>
+        <form
+          className="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            const textValue = this.textInput.current?.value;
+            this.setState({
+              textIsValid: textValue
+                ? checkValidation(textValue, validation.textInput.regExp)
+                : null,
+            });
+          }}
+        >
+          <TextInput passedRef={this.textInput}>
+            {typeof textIsValid === 'boolean' && !textIsValid && (
+              <ErrorMessage errorText={validation.textInput.wrongMsg} />
+            )}
+          </TextInput>
+          {/* <DateInput />
+          <Select />
+          <CheckboxGroup />
+          <RadioGroup />
+          <Uploader /> */}
+          <Submit />
+        </form>
+      </>
     );
   }
 }
--- src/components/form/TextInput.tsx
+++ src/components/form/TextInput.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Children } from 'react';
 
-class TextInput extends React.Component {
+type TextInputProps = {
+  passedRef: React.RefObject<HTMLInputElement>;
+  children: React.ReactNode;
+};
+
+class TextInput extends React.Component<TextInputProps> {
   render(): React.ReactNode {
-    return <input type={'text'} className="input-text input" />;
+    console.log(this.props.passedRef.current?.value);
+    return (
+      <div className="field">
+        <label className="label text-label" htmlFor="name">
+          1.Write your name,surname and zip-code
+        </label>
+        <input
+          id="name"
+          type={'text'}
+          className="input-text input"
+          placeholder="Type here your name and zip-code..."
+          ref={this.props.passedRef}
+          required
+        />
+        {this.props.children}
+      </div>
+    );
   }
 }
 
