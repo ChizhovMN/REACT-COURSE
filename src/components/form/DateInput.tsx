c40fd7228b3f716046e58de49ddb8a16ae4308b8
Chizhov Maxim <chizhov98ip@gmail.com>
2023-03-22 20:43:32 +0300

    feat:add date input with validation

--- src/components/form/DateInput.tsx
+++ src/components/form/DateInput.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { InputProps } from 'types';
+import { LABEL } from '../../helpers/const';
+import Label from './Label';
 
-class DateInput extends React.Component {
+class DateInput extends React.Component<InputProps> {
   render(): React.ReactNode {
-    return <input type={'date'} className="input-date input" />;
+    return (
+      <div className="field">
+        <Label
+          htmlFor={LABEL.DATE_INPUTE.htmlFor}
+          classNames={LABEL.DATE_INPUTE.classNames}
+          labelText={LABEL.DATE_INPUTE.labelText}
+        />
+        <input
+          id={LABEL.DATE_INPUTE.htmlFor}
+          type={'date'}
+          className="input-date input"
+          ref={this.props.passedRef}
+          required
+        />
+        {this.props.children}
+      </div>
+    );
   }
 }
 
