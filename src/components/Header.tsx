3559f8f9cbb4430822e63359dab0436aea261dcf
Chizhov Maxim <chizhov98ip@gmail.com>
2023-03-26 10:30:40 +0300

    refactor:change header component from class to functional

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { withRouter, WithRouterProps } from '../hocs/withRouter';
+import { useLocation } from 'react-router-dom';
 import HeaderNav from './HeaderNav';
 
-class Header extends React.Component<WithRouterProps> {
-  render(): React.ReactNode {
-    return (
-      <header className="header">
-        <HeaderNav />
-        <div>Current Page:{this.props.location.pathname}</div>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  const location = useLocation();
+  return (
+    <header className="header">
+      <HeaderNav />
+      <div>Current Page:{location.pathname}</div>
+    </header>
+  );
+};
 
-export default withRouter(Header);
+export default Header;
--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class HeaderNav extends React.Component {
-  render() {
-    return (
-      <nav className="nav">
-        <Link to="/">Main Page</Link>
-        <Link to="/about_us">About Us</Link>
-        <Link to="/form">Form</Link>
-      </nav>
-    );
-  }
-}
-
+const HeaderNav = () => {
+  return (
+    <nav className="nav">
+      <Link to="/">Main Page</Link>
+      <Link to="/about_us">About Us</Link>
+      <Link to="/form">Form</Link>
+    </nav>
+  );
+};
 export default HeaderNav;
