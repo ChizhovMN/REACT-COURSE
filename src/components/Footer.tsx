0eafd560b94eceef5b795533b585ca2a4dc1f718
Chizhov Maxim <chizhov98ip@gmail.com>
2023-03-26 10:31:47 +0300

    refactor:change footer to functional component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Footer extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <footer className="footer">
-        <Link to="https://github.com/ChizhovMN?tab=repositories" target="_blank">
-          github
-        </Link>
-        {'  '}
-        2023 year{'  '}
-        <Link to="https://rs.school/" target="_blank">
-          rss school
-        </Link>
-      </footer>
-    );
-  }
-}
+const Footer = () => (
+  <footer className="footer">
+    <Link to="https://github.com/ChizhovMN?tab=repositories" target="_blank">
+      github
+    </Link>
+    {'  '}
+    2023 year{'  '}
+    <Link to="https://rs.school/" target="_blank">
+      rss school
+    </Link>
+  </footer>
+);
 
 export default Footer;
