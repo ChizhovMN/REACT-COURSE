686280c70c91ad34192c2bab187a71cf7dcd76bd
Chizhov Maxim <chizhov98ip@gmail.com>
2023-03-26 10:39:36 +0300

    refactor: main card to functional component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { CardType } from 'types';
 
-class Card extends React.Component<Partial<CardType>> {
-  render(): React.ReactNode {
-    return (
-      <div className="card-item">
-        <div className="card-img">
-          <div className="img-overflow">
-            <img src={this.props.thumbnail} alt={this.props.brand} className="img" loading="lazy" />
-          </div>
-          <div className="category">{this.props.category}</div>
-          <div className="brand">{this.props.brand}</div>
-          <div className="tittle">{this.props.title}</div>
-          <div className="stock">Stock:{this.props.stock}</div>
-        </div>
-        <div className="info">
-          <div>Rating:{this.props.rating}</div>
-          <div>Discount:{this.props.discountPercentage}</div>
-          <div>Price:{this.props.price}</div>
-        </div>
+const Card: FunctionComponent<CardType> = ({
+  thumbnail,
+  brand,
+  category,
+  title,
+  stock,
+  rating,
+  discountPercentage,
+  price,
+}) => (
+  <div className="card-item">
+    <div className="card-img">
+      <div className="img-overflow">
+        <img src={thumbnail} alt={brand} className="img" loading="lazy" />
       </div>
-    );
-  }
-}
-
+      <div className="category">{category}</div>
+      <div className="brand">{brand}</div>
+      <div className="tittle">{title}</div>
+      <div className="stock">Stock:{stock}</div>
+    </div>
+    <div className="info">
+      <div>Rating:{rating}</div>
+      <div>Discount:{discountPercentage}</div>
+      <div>Price:{price}</div>
+    </div>
+  </div>
+);
 export default Card;
