2f9f50ea592acf05395c4f9ff3acc6c2cc44b962
Chizhov Maxim <chizhov98ip@gmail.com>
2023-03-26 11:32:28 +0300

    refactor:change 404 page to functional

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 
-class Page404 extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <div className="error-page">
-        <p className="error-header">404 page</p>
-        <p className="error-subheader">Oops! Not found!</p>
-      </div>
-    );
-  }
-}
-
+const Page404 = () => (
+  <div className="error-page">
+    <p className="error-header">404 page</p>
+    <p className="error-subheader">Oops! Not found!</p>
+  </div>
+);
 export default Page404;
