cf7546c8e26b0f41e66df36b8a8f134401ee9bb7
Chizhov Maxim <chizhov98ip@gmail.com>
2023-04-04 20:22:52 +0300

    refactor:delete json data from previous task,delete unusable props

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,16 @@ import AboutPage from './pages/About';
 import Page404 from './pages/404';
 import FormPage from './pages/FormPage';
 import Footer from './components/Footer';
-import { ProductsType } from 'types';
 import ErrorBoundary from './components/ErrorBoundary';
 
-const App: FunctionComponent<ProductsType> = ({ products }) => {
+const App: FunctionComponent = () => {
   return (
     <>
       <Header />
       <main className="main">
         <ErrorBoundary>
           <Routes>
-            <Route path="/" element={<MainPage products={products} />} />
+            <Route path="/" element={<MainPage />} />
             <Route path="/about_us" element={<AboutPage />} />
             <Route path="/form" element={<FormPage />}></Route>
             <Route path="/404" element={<Page404 />} />
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import Cards from './data.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App products={Cards.products} />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
